db967456e5a1bab0030ca29ada59e08fba4eecc7
Evann Regnault <estym.yt@gmail.com>
2020-09-23 17:29:34 +0200

    Create infos.js

---
+++ infos.js
@@ -0,0 +1,11 @@
+{
+  "name": "TSD Web Interface",
+  "tags": [
+    "Python",
+    "Flask",
+    "EJS",
+    "HTML",
+    "CSS"
+  ],
+  "text":"Une interface web pour pouvoir analyser un serveur terraria à l'aide de l'API du mod Terraria Server Dashboard."
+}
